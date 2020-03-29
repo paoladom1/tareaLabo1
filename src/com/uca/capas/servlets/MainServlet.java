60fdba42ffad36f99cb0eb7431a3fbe1b8cb7d65
Paola Dominguez <paoladomdiaz@gmail.com>
2020-03-29 15:52:13 -0600

    Tarea 1

---
+++ src/com/uca/capas/servlets/MainServlet.java
@@ -0,0 +1,60 @@
+package com.uca.capas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String user = request.getParameter("user");
+		String password = request.getParameter("pass");
+		
+		PrintWriter out = response.getWriter();
+		
+		if("paola".equals(user) && "1234".equals(password)) {
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<h3>" + "ACCESO CORRECTO" + "</h3>");
+			out.println("</body>");
+			out.println("</html>");
+		} else {
+			out.println("<html>");
+			out.println("<body>");
+			out.println("<h3>" + "ACCESO INCORRECTO" + "</h3>");
+			out.println("</body>");
+			out.println("</html>");
+		}
+	}
+
+}
